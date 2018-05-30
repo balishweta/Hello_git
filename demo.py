4336fc65c60dac8c3e0ea66bb1c8f80f16a68cbb
balishweta <39703620+balishweta@users.noreply.github.com>
2018-05-30 16:03:53 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,32 @@
+#1.print something
+print("Python is a programming Language")
+
+
+#2. join strings
+a="Shweta"
+b="Sharma"
+print(a+b)
+
+# 3. print variable values
+x=input("enter the first number")
+y=input("enter the second number")
+z=input("enter the third number")
+print("first number is",x)
+print("second number is",y)
+print("third number is",z)
+
+#4. lets get started
+print("""let's" get started""")
+
+
+
+#5. print using placeholders
+print('''s="acadview"''')
+print('''course="python"''')
+print('''fees=5000''')
+
+# 6. fill in the blanks
+name="tony stark"
+salary=100000
+print('%s''%d' % (name, salary))
+
